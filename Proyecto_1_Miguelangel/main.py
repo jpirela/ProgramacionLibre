3ff97feea7354c1210d27f64a86f0470771f0e15
mcg2027 <miguelangelcontrerasmcg@mail.com>
2025-05-30 15:23:40 -0400

    Proyecto_1_Miguelangel

---
+++ Proyecto_1_Miguelangel/main.py
@@ -0,0 +1,22 @@
+# Miguelangel Contreras C.I = 30.643.509
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GLib
+
+GLADE_FILE = "Proyecto_1_Miguelangel.glade"
+
+def on_window_destroy(window, data=None):
+    Gtk.main_quit()
+
+def cargar_glade():
+    builder = Gtk.Builder()
+    builder.add_from_file(GLADE_FILE)
+
+    window = builder.get_object("main_window")
+    window.connect("destroy", on_window_destroy)
+
+    window.show_all()
+
+if __name__ == "__main__":
+    cargar_glade()
+    Gtk.main()
